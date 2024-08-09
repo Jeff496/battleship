ba742d52402aac2ae2e5939d78e3e1d92ec24ea7
Jeffrey Keem <keemaje5@gmail.com>
2024-08-09 00:33:04 -0700

    Project setup files

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devtool: "source-map",
+  mode: "production",
+  watch: true,
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
