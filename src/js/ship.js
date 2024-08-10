f5b58174fe6f1d2f8b935246752a70704fb23778
Jeffrey Keem <keemaje5@gmail.com>
2024-08-10 00:50:51 -0700

    ship.js implementation and tests

---
+++ src/js/ship.js
@@ -0,0 +1,30 @@
+const shipTypes = {
+  CARRIER: 5,
+  BATTLESHIP: 4,
+  CRUISER: 3,
+  SUBMARINE: 3,
+  DESTROYER: 2,
+};
+
+class Ship {
+  constructor(length, hitCount) {
+    this.length = length;
+    this.hitCount = hitCount;
+    this.sunk = false;
+  }
+  hit() {
+    this.hitCount++;
+    this.isSunk();
+  }
+
+  isSunk() {
+    if (this.length === this.hitCount) {
+      this.sunk = true;
+    }
+  }
+}
+
+module.exports = {
+  Ship,
+  shipTypes,
+};
---
+++ src/tests/ship.test.js
@@ -0,0 +1,22 @@
+const { Ship, shipTypes } = require("../js/ship.js");
+
+test("ship constructor", () => {
+  const shipTest = new Ship(shipTypes.DESTROYER, 1);
+  expect(shipTest.length).toBe(2);
+  expect(shipTest.hitCount).toBe(1);
+});
+
+test("hit function", () => {
+  const shipTest = new Ship(shipTypes.BATTLESHIP, 2);
+  shipTest.hit();
+  expect(shipTest.hitCount).toBe(3);
+  shipTest.hit();
+  expect(shipTest.hitCount).toBe(4);
+  expect(shipTest.sunk).toBe(true);
+});
+
+test("isSunk function", () => {
+  const shipTest = new Ship(shipTypes.CARRIER, 5);
+  shipTest.isSunk();
+  expect(shipTest.sunk).toBe(true);
+});
